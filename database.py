3cebc73bc9899a6ae861b430b9c99d8173a51edd
Shiva3505 <shiva03505@gmail.com>
2024-11-22 17:53:00 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,12 @@
+import pymysql
+from tkinter import messagebox
+
+def connect_database(database=''):
+    try:
+        conn = pymysql.connect(host='localhost', user='root', password='Shiva3505@',database="ims1_data1")
+        mycursor = conn.cursor()
+    except:
+        messagebox.showerror('Error', 'Something went wrong, Please open MySQL app before running again')
+        return None,None
+
+    return mycursor, conn
